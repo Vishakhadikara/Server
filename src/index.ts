eb6c1a9bfd036d5fb10e296290d14490ed9b65b9
VIZBLIZ <44446283+Vishakhadikara@users.noreply.github.com>
2023-05-20 23:59:10 +0530

    port update

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
-import { listen } from "@colyseus/arena";
-import arenaConfig from "./arena.config";
-listen(arenaConfig);
+import express from 'express';
+import{createServer} from 'http';
+import { Server } from 'colyseus';
+import { GameRoom } from "./gameroom";
+import{monitor} from '@colyseus/monitor';
+
+const port = Number(process.env.PORT || 2567);
+const app = express();
+
+const gameServer = new Server({
+    server: createServer(app)
+});
+
+gameServer.define('gameRoom', GameRoom);
+
+gameServer.onShutdown(function(){
+     console.log(`game server is going down.`);
+});
+
+gameServer.listen(port)
+console.log(`Listening:${ port }`);
